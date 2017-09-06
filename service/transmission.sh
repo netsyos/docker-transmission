0ce4636c6bffd1d39aac9ac4b29675a1ec9d8b0e
gael-cosnard <gcd@myfox.fr>
2017-09-06 08:55:41 +0200

    Change service start

--- service/transmission.sh
+++ service/transmission.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-service transmission-daemon stop
-exec sudo -u debian-transmission transmission-daemon --log-debug --config-dir /etc/transmission-daemon --logfile /var/log/transmission.log
+exec sudo -u debian-transmission -g debian-transmission /usr/bin/transmission-daemon --log-debug --config-dir /etc/transmission-daemon/ --logfile /var/log/transmission.log
 sleep 2
